ce643b0561f6b26455e63629b3f433a2aa65003d
Hector <norzagaray@me.com>
2018-01-17 22:55:58 -0800

    tests run some fail

--- lab-hector/__test__/reader.test.js
+++ lab-hector/__test__/reader.test.js
@@ -1,129 +1,44 @@
 'use strict';
 
-const reader = require(`../lib/reader.js`);
+const reader = require('../lib/reader.js');
 require('jest');
 
 let arrayOne = [`${__dirname}/../assets/text1.txt`, `${__dirname}/../assets/text2.txt`, `${__dirname}/../assets/text3.txt`];
+
 let arrayTwo = ['file one', 'file two', 'file three'];
 
 describe('reader module', function () {
   describe('#reader', function () {
-    it('should return an array (object)', function () {
-      return reader(arrayOne, (err, fd) => {
+    it('should return an array (object)', (done) => {
+       reader(arrayOne, (err, fd) => {
         if (err) console.error(err);
-        expect(typeof fd).toBe('object');
+        expect(fd).toBeInstanceOf(Array);
+        done();
       });
     });
-    it('should return strings', function () {
-      return reader(arrayOne, (err, fd) => {
+
+
+    it('should return strings', function (done) {
+       reader(arrayOne, (err, fd) => {
         if (err) console.error(err);
         expect(typeof fd[0]).toBe('string');
+      done();
       });
     });
-    it('should return the strings in the same order', function () {
-      return reader(`${__dirname}/../assets/text1.txt`, (err, fd) => {
+    it('should return the strings in the same order', function (done) {
+       reader(`${__dirname}/../assets/text1.txt`, (err, fd) => {
         if (err) console.error(err);
         expect(fd[0].includes('#1')).toBe(true);
+        done();
       });
-      return reader(arrayOne, (err, fd) => {
-        if (err) console.error(err);
-        expect(fd[2].includes('#3')).toBe(true);
-      });
-      return reader(arrayOne, (err, fd) => {
-        if (err) console.error(err);
-        expect(fd[1].includes('#2')).toBe(true);
-      });
+
     });
-    it('should throw an error if the input is not valid file paths', function () {
-      return reader(arrayTwo, (err, fd) => {
+    it('should throw an error if the input is not valid file paths', function (done) {
+       reader(arrayTwo, (err, fd) => {
         if (err) console.error(err);
         expect(reader()).toThrowError();
+        done();
       });
     });
   });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const reader = require(`../lib/reader.js`);
-// require('jest');
-
-// let arrayOne = [`${__dirname}/../assets/text1.txt`, `${__dirname}/..assets/text2.txt`, `${__dirname}/../assets/text3.txt`];
-// let arrayTwo = ['file one', 'file two', 'file three'];
-
-// describe('reader module', function () {
-//   describe('#reader', function () {
-//     it('should return an array(object)', function () {
-//       return reader(arrayOne, (err, fd) => {
-//         if (err) console.log(err);
-//         expect(typeof fd).toBe('object');
-//       });
-//     });
-//     it('should return strings', function () {
-//       return reader(arrayOne, (err, fd) => {
-//         if (err) console.log(err);
-//         expect(typeof fd[0].toBe('string'));
-//       });
-//     });
-//     it('should return the strings in the same order', function () {
-//       return reader(arrayOne, (err, fd) => {
-//         if (err) console.log(err);
-//         expect(fd[0].includes('#1')).toBe(true);
-//       });
-//       return reader(arrayOne, (err, fd) => {
-//         if (err) console.log(err);
-//         expect(fd[1].includes('#2')).toBe(true);
-//       });
-//       return reader(arrayOne, (err, fd) => {
-//         if (err) console.log(err);
-//         expect(fd[2].includes('#3')).toBe(true);
-//       });
-//     });
-//     it('should throw an error if the input is not valid file paths', function () {
-//       return reader(arrayTwo, (err, fd) => {
-//         if (err) console.log(err);
-//         expect(reader()).toThrow();
-//       });
-//     });
-//   });
-// });
